a4e25782fe31b3c1c9726ac2fea9451b5d9fab8a
kprevas <kprevas@101b4684-a21a-0410-927c-ab660e198cc0>
2006-08-10 07:51:49 +0000

    add files missed in last checkin

---
+++ trunk/ClefDraw.m
@@ -0,0 +1,36 @@
+//
+//  ClefDraw.m
+//  Señor Staff
+//
+//  Created by Konstantine Prevas on 7/29/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import "ClefDraw.h"
+#import "Clef.h"
+
+@implementation ClefDraw
+
++(void)draw:(Clef *)clef atX:(NSNumber *)x base:(NSNumber *)baseY highlighted:(BOOL)highlighted{
+	NSImage *img;
+	NSPoint clefLoc;
+	clefLoc.x = [x floatValue];
+	if(clef == [Clef trebleClef]){
+		if(highlighted){
+			img = [NSImage imageNamed:@"treble over.png"];
+		} else{
+			img = [NSImage imageNamed:@"treble.png"];
+		}
+		clefLoc.y = [baseY floatValue] + 20;
+	} else if(clef == [Clef bassClef]){
+		if(highlighted){
+			img = [NSImage imageNamed:@"bass over.png"];
+		} else{
+			img = [NSImage imageNamed:@"bass.png"];
+		}
+		clefLoc.y = [baseY floatValue] - 7;
+	}
+	[img compositeToPoint:clefLoc operation:NSCompositeSourceOver];
+}
+
+@end
