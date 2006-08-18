77e2e466eba0e9f1de1b949e0c8776e3e5c9f019
kprevas <kprevas@101b4684-a21a-0410-927c-ab660e198cc0>
2006-08-18 05:07:13 +0000

    added unit tests for NoteBase.m

--- trunk/NoteTest.m
+++ trunk/NoteTest.m
@@ -1,14 +1,31 @@
 //
 //  NoteTest.m
-//  Señor Staff
+//  Se√±or Staff
 //
 //  Created by Konstantine Prevas on 8/17/06.
 //  Copyright 2006 Konstantine Prevas. All rights reserved.
 //
 
 #import "NoteTest.h"
-
+#import "Note.h"
 
 @implementation NoteTest
+
+- (void)setUp{
+	note = [[Note alloc] initWithPitch:4 octave:3 duration:2 dotted:YES accidental:SHARP];
+}
+- (void)tearDown{
+	[note release];
+}
+
+- (void) testGetEffectiveDuration{
+	STAssertEquals([note getEffectiveDuration], (float)0.75, @"Wrong duration returned for note.");
+}
+
+- (void) testRemoveDuration{
+	NSArray *array = [note removeDuration:0.375];
+	STAssertEquals([note getEffectiveDuration], (float)0.375, @"Wrong duration left after removing duration from note.");
+	STAssertEquals([array count], (unsigned)1, @"Wrong number of notes split off when removing duration.");
+}
 
 @end
