6a4e0d9b95302f9d61cd0c9944b25c9e1669b223
kprevas <kprevas@gmail.com>
2006-08-18 05:07:13 +0000

    added unit tests for NoteBase.m

--- NoteTest.h
+++ NoteTest.h
@@ -1,16 +1,20 @@
 //
 //  NoteTest.h
-//  Señor Staff
+//  Se√±or Staff
 //
 //  Created by Konstantine Prevas on 8/17/06.
 //  Copyright 2006 Konstantine Prevas. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
-
+@class Note;
 
 @interface NoteTest : SenTestCase {
-
+	Note *note;
 }
+
+- (void) testGetEffectiveDuration;
+
+- (void) testRemoveDuration;
 
 @end
