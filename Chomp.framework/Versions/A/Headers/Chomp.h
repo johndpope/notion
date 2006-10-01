293b5c36b1f309f56097422accc48333ba4b351c
kprevas <kprevas@gmail.com>
2006-10-01 23:12:14 +0000

    added CHOMP framework

---
+++ Chomp.framework/Versions/A/Headers/Chomp.h
@@ -0,0 +1,10 @@
+//
+//  CHOMP.h
+//  Chomp
+//
+//  Created by Michael Ash on 12/16/04.
+//
+
+#import "NSCollections+CHOMPIteration.h"
+#import "NSObject+CHOMPIteration.h"
+#import "NSObject+CHOMPHOM.h"
---
+++ Chomp.framework/Versions/A/Headers/NSObject+CHOMPIteration.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+CHOMPIteration.h
+//  Chomp
+//
+//  Created by Michael Ash on 12/16/04.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSObject (CHOMPIteration)
+
+- collectSelf;
+- doSelf;
+
+@end
