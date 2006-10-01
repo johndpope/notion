9b40697ba35ecc7c97f742cc234c56e845f2c63e
kprevas <kprevas@101b4684-a21a-0410-927c-ab660e198cc0>
2006-10-01 23:12:14 +0000

    added CHOMP framework

---
+++ trunk/Chomp.framework/Versions/A/Headers/NSCollections+CHOMPIteration.h
@@ -0,0 +1,34 @@
+//
+//  NSCollections+CHOMPIteration.h
+//  Chomp
+//
+//  Created by Michael Ash on 12/16/04.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSArray (CHOMPIteration)
+
+- collect;
+- do;
+- each;
+
+@end
+
+@interface NSSet (CHOMPIteration)
+
+- collect;
+- do;
+- each;
+
+@end
+
+@interface NSDictionary (CHOMPIteration)
+
+- collect;
+- do;
+- each;
+
+@end
+
