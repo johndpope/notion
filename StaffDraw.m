074b2eb3b902752807916889eba21a319f7fa0bc
kprevas <kprevas@gmail.com>
2006-10-21 23:45:46 +0000

    draw measures with notes tied to by visible notes, even if measure is outside visible area

--- StaffDraw.m
+++ StaffDraw.m
@@ -12,14 +12,27 @@
 
 @implementation StaffDraw
 
+static NSMutableArray *mustDraw;
+
 + (void) draw:(Staff *)staff inView:(NSView *)view target:(id)target targetLocation:(NSPoint)location mode:(NSDictionary *)mode{
+	if(mustDraw == nil){
+		mustDraw = [[NSMutableArray array] retain];
+	}
+	[mustDraw removeAllObjects];
 	NSEnumerator *measures = [[staff getMeasures] objectEnumerator];
 	id measure;
 	while(measure = [measures nextObject]){
-		if([view needsToDrawRect:[MeasureController boundsOf:measure]]){
+		if([view needsToDrawRect:[MeasureController boundsOf:measure]] || [mustDraw containsObject:measure]){
 			[[measure getViewClass] draw:measure target:target targetLocation:location mode:mode];
 		}
 	}	
+}
+
++ (void) mustDraw:(Measure *)measure{
+	if(mustDraw == nil){
+		mustDraw = [[NSMutableArray array] retain];
+	}
+	[mustDraw addObject:measure];
 }
 
 @end
