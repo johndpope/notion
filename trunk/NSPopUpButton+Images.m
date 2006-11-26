eb96f966f6931454e10d9a80134957b669a3aa62
kprevas <kprevas@101b4684-a21a-0410-927c-ab660e198cc0>
2006-11-26 22:23:09 +0000

    fix licensing text

--- trunk/NSPopUpButton+Images.m
+++ trunk/NSPopUpButton+Images.m
@@ -1,9 +1,10 @@
 //
 //  NSPopUpButton+Images.m
-//  Señor Staff
 //
 //  Created by Konstantine Prevas on 11/26/06.
-//  Copyright 2006 Konstantine Prevas. All rights reserved.
+//
+//  The source code in this file is available in the public domain.  Permission is granted to use,
+//  modify, or distribute it; however, it is provided "as is" and without warranties as to performance or merchantability.
 //
 
 #import "NSPopUpButton+Images.h"
