bdb1fdb7450423a5d293b4e5c105d7c24295d565
kprevas <kprevas@gmail.com>
2006-12-16 21:17:02 +0000

    licensing text

--- ToolbarHelperController.h
+++ ToolbarHelperController.h
@@ -3,8 +3,9 @@
 //  Señor Staff
 //
 //  Created by Konstantine Prevas on 11/19/06.
-//  Copyright 2006 Konstantine Prevas. All rights reserved.
 //
+//  The source code in this file is available in the public domain.  Permission is granted to use,
+//  modify, or distribute it; however, it is provided "as is" and without warranties as to performance or merchantability.
 
 #import <Cocoa/Cocoa.h>
 
