06c25671acb664211ace9fe43c4896211e4aadaf
kprevas <kprevas@101b4684-a21a-0410-927c-ab660e198cc0>
2007-01-05 06:50:50 +0000

    performance improvement

--- trunk/TimeSignatureController.m
+++ trunk/TimeSignatureController.m
@@ -10,11 +10,23 @@
 #import "TimeSignatureDraw.h"
 #import "TimeSignature.h"
 
+static float widths[17] = {
+	0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0,
+	0.0, 0.0, 0.0, 0.0, 0.0
+};
+
 @implementation TimeSignatureController
 
 + (float) widthOf:(TimeSignature *)timeSig{
-	return [timeSig isKindOfClass:[NSNull class]] ? 10.0 : 
-		[[NSString stringWithFormat:@"%d", [timeSig getBottom]] sizeWithAttributes:[TimeSignatureDraw timeSigStringAttrs]].width + 10.0;
+	if([timeSig isKindOfClass:[NSNull class]]){
+		return 10.0;
+	}
+	if(widths[[timeSig getBottom]] == 0.0) {
+		widths[[timeSig getBottom]] = [[NSString stringWithFormat:@"%d", [timeSig getBottom]] sizeWithAttributes:[TimeSignatureDraw timeSigStringAttrs]].width + 10.0;
+	}
+	return widths[[timeSig getBottom]];
 }
 
 + (void)handleMouseClick:(NSEvent *)event at:(NSPoint)location on:(TimeSigTarget *)sig mode:(NSDictionary *)mode view:(ScoreView *)view{
